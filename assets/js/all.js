f732bedc569a4d9423098b1c6c0fad8ef1120f4b
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-07-25 04:05:56 -0300

    Criado arquivo js

---
+++ assets/js/all.js
@@ -0,0 +1,13 @@
+// window.onscroll = () => {
+//     sumario = document.querySelector("#sumario");
+//     if(window.pageYOffset > 181) {
+//         sumario.style.top = 0;
+//     }
+//     if(window.pageYOffset < 181){
+//         sumario.style.top = 182;
+//     }
+// };
+$('body').scrollspy({ target: '#sumario' })
+$('[data-spy="scroll"]').each(function () {
+    var $spy = $(this).scrollspy('refresh')
+  })
